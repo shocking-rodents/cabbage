453e6d5c68ed7d8fcf1b3d0377c38c9842b610b8
Andrey Zakharov <andrey.v.zakharov@yandex.ru>
2017-12-14 18:54:55 +0300

    Fix export

--- cabbage/__init__.py
+++ cabbage/__init__.py
@@ -1,2 +1,2 @@
-from .amqp import ServiceUnavailableError, AmqpConnection, AbstractAsyncRpcServer, AsyncAmqpRpcServer, \
-    AbstractAsyncRpcClient, AsyncAmqpRpcClient
+# -*- coding: utf-8 -*-
+from .amqp import ServiceUnavailableError, AmqpConnection, AbstractAsyncRpcServer, AbstractAsyncRpcClient, AsyncAmqpRpc  # noQA
