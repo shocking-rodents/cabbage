34d46e0ffc216c44f5ec712198cca8e78015fd19
Andrey Zakharov <andrey.v.zakharov@yandex.ru>
2018-03-04 17:16:44 +0300

    Add pytest-asyncio

---
+++ test/conftest.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+# All test coroutines will be treated as marked.
+pytestmark = pytest.mark.asyncio
---
+++ test/test_amqp.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+import asyncio
+
+
+async def test_test(event_loop):
+    await asyncio.sleep(0, loop=event_loop)
