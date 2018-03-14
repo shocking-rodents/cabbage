6c9aada8a28bdcc54e8e157b9cf6d96d2d80e983
Andrey Zakharov <andrey.v.zakharov@yandex.ru>
2018-03-15 00:17:26 +0300

    Add subscription integration test

--- tests_integration/conftest.py
+++ tests_integration/conftest.py
@@ -1,4 +1,54 @@
 # -*- coding: utf-8 -*-
 import logging
+from urllib.parse import quote
+
+import pytest
+import requests
+from requests.auth import HTTPBasicAuth
+
+from cabbage import AmqpConnection, AsyncAmqpRpc
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [%(levelname)s] %(name)s: %(message)s')
+
+
+class Management:
+    def __init__(self, url):
+        self.base_url = url
+        self.kwargs = dict(
+            auth=HTTPBasicAuth('guest', 'guest'),
+            headers={'content-type': 'application/json'},
+        )
+
+    def get_queue(self, vhost, name):
+        return requests.get(f'{self.base_url}/api/queues/{quote(vhost, safe="")}/{quote(name, safe="")}',
+                            **self.kwargs).json()
+
+    def put_vhost(self, vhost):
+        requests.put(f'{self.base_url}/api/vhosts/{quote(vhost, safe="")}', **self.kwargs)
+        requests.put(f'{self.base_url}/api/permissions/{quote(vhost, safe="")}/guest',
+                     json={'configure': '.*', 'write': '.*', 'read': '.*'}, **self.kwargs)
+
+    def delete_vhost(self, vhost):
+        requests.delete(f'{self.base_url}/api/vhosts/{quote(vhost, safe="")}', **self.kwargs)
+
+
+@pytest.fixture(scope='session')
+def management():
+    """Wrapper for RabbitMQ Management Plugin API."""
+    return Management('http://rabbitmq:15672')
+
+
+@pytest.yield_fixture(scope='function', autouse=True)
+def vhost_environment(request, management):
+    management.put_vhost(request.node.name)
+    yield
+    management.delete_vhost(request.node.name)
+
+
+@pytest.fixture
+async def rpc(request, event_loop):
+    """Ready-to-work RPC connected to RabbitMQ in Docker."""
+    connection = AmqpConnection(host='rabbitmq', port=5672, virtualhost=request.node.name, loop=event_loop)
+    rpc = AsyncAmqpRpc(connection=connection, request_handler=lambda x: x)
+    await rpc.connect()
+    return rpc
--- tests_integration/test_server.py
+++ tests_integration/test_server.py
@@ -6,7 +6,25 @@ from cabbage import AmqpConnection, AsyncAmqpRpc
 pytestmark = pytest.mark.asyncio
 
 
-async def test_ok():
+async def test_sanity(management):
+    """Basic sanity check. If this fails, something is horribly wrong (or you are not running in Docker)."""
     connection = AmqpConnection(host='rabbitmq', port=5672)
     rpc = AsyncAmqpRpc(connection=connection)
     await rpc.connect()
+    assert management.get_queue('/', 'my_crappy_queue_that_should_not_exist') == {
+        'error': 'Object Not Found',
+        'reason': '"Not Found"\n'
+    }
+
+
+@pytest.mark.parametrize('exchange, queue', [
+    ('', 'my_queue'),
+    ('my_exchange', 'my_queue'),
+])
+async def test_subscribe(request, rpc, management, exchange, queue):
+    assert management.get_queue(request.node.name, queue) == {
+        'error': 'Object Not Found',
+        'reason': '"Not Found"\n'
+    }
+    await rpc.subscribe(exchange=exchange, queue=queue)
+    assert management.get_queue(request.node.name, queue).get('name') == queue
